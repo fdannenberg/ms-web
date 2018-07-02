f1e1f7d3971bef32e3ca7830c092906f1fc60074
Frits Dannenberg <iris@localhost.localdomain>
2018-07-01 23:37:44 -0700

    table styling

--- annealserv/bin/app.py
+++ annealserv/bin/app.py
@@ -23,7 +23,7 @@ class Index(object):
         form = web.input(substrate="Nameless job", sequence="AGCGTGA")
                 
         if len(form.sequence) > MAX_SEQ_LEN or len(form.sequence) < 3 :
-            return render.errorpage(greeting=form.sequence, substrate=form.substrate, result=1e-36)
+            return render.errorpage(sequence=form.sequence, substrate=form.substrate, result=1e-36)
         
 #         if (time.time() - prev_time) < 8.0:
 #             return render.errorpage(greeting="Please do not submit jobs this quickly!", substrate=form.substrate, result=1e-36)
@@ -35,12 +35,12 @@ class Index(object):
 
         except Exception as e:
             print str(e)
-            return render.errorpage(greeting=form.sequence, substrate="Exception", result=1e-36)
+            return render.errorpage(sequence=form.sequence, substrate="Exception", result=1e-36)
 
         if float(float(form.result['rate'])) < 10.0 ** -30:
-            return render.errorpage(greeting=form.sequence, substrate=form.substrate, result=1e-36)
+            return render.errorpage(sequence=form.sequence, substrate=form.substrate, result=1e-36)
         
-        return render.index(greeting=form.sequence, substrate=form.substrate, result=form.result)
+        return render.index(sequence=form.sequence, substrate=form.substrate, result=form.result)
 
 
 if __name__ == "__main__":
